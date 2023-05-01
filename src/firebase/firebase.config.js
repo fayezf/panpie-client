811e98a0f25a503e10a6cca61b842e36d2fa2a3a
fayezf <mohammadfayezahmed835@gmail.com>
2023-05-01 23:02:25 +0600

    router and firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQrr_SO4PpymQO4Fn-huT3pY8CDq4gxt0",
+  authDomain: "chef-hunter-client-6efb9.firebaseapp.com",
+  projectId: "chef-hunter-client-6efb9",
+  storageBucket: "chef-hunter-client-6efb9.appspot.com",
+  messagingSenderId: "247154834135",
+  appId: "1:247154834135:web:5373362eb318cf07178e92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
