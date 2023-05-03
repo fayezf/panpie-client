7fd036f0ec01dec6e09ed563f7e62a49bce059fa
fayezf <mohammadfayezahmed835@gmail.com>
2023-05-03 16:14:27 +0600

    ratings and likes added

--- src/pages/Recipe/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe/Recipe.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
-import { FaRegThumbsUp } from 'react-icons/fa';
+import { FaRegStar, FaRegThumbsUp, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 import { useLoaderData } from 'react-router-dom';
 
 const Recipe = () => {
     const recipe = useLoaderData();
-    const {id, chefPicture, chefName, likes, ingredients} = recipe;
+    const { id, chefPicture, chefName, ratings, likes, ingredients } = recipe;
     return (
-        <Card>
-            <Card.Img variant="top" src={chefPicture} />
-            <Card.Body>
-                <Card.Title>{chefName}</Card.Title>
-                <Card.Text>
-                    <ul>
-                        <li>{ingredients}</li>
-                    </ul>
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            <Card.Footer>
-                <div>
-                <FaRegThumbsUp /> {likes}
-                </div>
-            </Card.Footer>
-        </Card>
+        <div className='mt-4'>
+            <h3>Recipe Category</h3>
+            <Card>
+                <Card.Img variant="top" src={chefPicture} />
+                <Card.Body>
+                    <Card.Title>{chefName}</Card.Title>
+                    <Card.Text>
+                        <ul>
+                            <li>{ingredients}</li>
+                        </ul>
+                    </Card.Text>
+                    <Button className='text-semibold' variant="success">Add to favorite</Button>
+                </Card.Body>
+                <Card.Footer className='text-muted d-flex'>
+                    <div className='flex-grow-1'>
+                        <Rating
+                            placeholderRating={ratings}
+                            readonly
+                            emptySymbol={<FaRegStar />}
+                            placeholderSymbol={<FaStar className='text-warning'></FaStar>}
+                            fullSymbol={<FaStar />}
+                        ></Rating>
+                        <span>{ratings}</span>
+                    </div>
+                    <div>
+                        <FaRegThumbsUp /> {likes}
+                    </div>
+                </Card.Footer>
+            </Card>
+        </div>
     );
 };
 
